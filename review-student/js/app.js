3a2e324fed095043a22c0f49eb05703d32e6c39d
Jeffrey McLuckie <jeffmcluckie1@gmail.com>
2021-11-19 19:19:48 -0500

    cant figure out changing problem number

--- review-student/js/app.js
+++ review-student/js/app.js
@@ -1,15 +1,4 @@
-
-
-
-    let randomProblem = [
-        {
-            left: Math.random() * (9 - 0) + 0,
-            right: Math.random() * (9 - 0) + 0
-        }
-    ]
-
-
-     /**
+ /**
       * todo make an array for 10 multiplication problems
     random number generation for problem Math.random(10) twice
 
@@ -22,7 +11,7 @@
        - Array.sort()
       */
 
-    buildNewProblem();
+     buildNewProblem();
 
 
       /** Functions called above
@@ -30,47 +19,96 @@
        * 
        */
 
+      let problemNumber = 1;
+      let numberCorrect = 0;
 
        function buildNewProblem() {
       
-
+        //Create element to house expression in div
         let expression = document.createElement('p');
+
+        //Create random numbers to act as left and right sides of multiplication problem
         let left = Math.floor((Math.random() * (9 - 0) + 0));
         let right = Math.floor((Math.random() * (9 - 0) + 0));
+
+        //Set correct answer equal to random numbers product
         let correctAnswer = left * right;
+
+        //Write problem to created element in string format
         expression.innerText =  left + " * " + right;
         let parent = document.getElementById('problem').querySelector('.expression');
         parent.insertAdjacentElement('beforeend', expression);
 
+        //Initialize array containing correct answers and 3 wrong answers
         let answers = [correctAnswer, getRandomNumber(), getRandomNumber(), getRandomNumber()];
 
-        let shuffledArrayOfAnswers = shuffleArray(answers);
-      
+        //Randomize order of possible answers in array
+        let array = shuffleArray(answers);
 
-        for (let i=0; i<shuffledArrayOfAnswers.length; i++){
-            
+        //Write answers from array to four li elements which IDs were created for
+        for (let i=0; i<array.length; i++){
+    
+            while(array[i] === array[i+1] || array[i] === array[i+2] || array[i] === array[i+3]){
+                //Do something (this is a stupid way to do this)
+                array = createAnswerArray();
+            }
             if (i === 0){
                 let firstLi = document.getElementById('one');
-                firstLi.innerText = shuffledArrayOfAnswers[0]; 
+                firstLi.innerText = array[0]; 
             } else if (i === 1){
                 let secondLi = document.getElementById('two');
-                secondLi.innerText = shuffledArrayOfAnswers[1]; 
+                secondLi.innerText = array[1]; 
             } else if (i === 2){
                 let thirdLi = document.getElementById('three');
-                thirdLi.innerText = shuffledArrayOfAnswers[2]; 
+                thirdLi.innerText = array[2]; 
             } else if (i === 3){
                 let fourthLi = document.getElementById('four');
-                fourthLi.innerText = shuffledArrayOfAnswers[3]; 
-            } else {
-                return null;
-            }
+                fourthLi.innerText = array[3]; 
+            } else return null;
         }
 
-        // shuffledArrayOfAnswers.forEach((answer) =>{
-        //     displayAnswer(answer);
-        // } ) 
-
+            let listOfAnswers = document.getElementById('answers').querySelector('ul').querySelectorAll('li');
+            listOfAnswers.forEach(element => element.addEventListener('click', (event) => {
+                console.log(event.target.innerText)
+                if(event.target.innerText == correctAnswer){
+                    event.target.setAttribute('class', 'correct-answer');
+                    numberCorrect += 1;
+                    problemNumber += 1;
+                    let problem = document.getElementById('currentProblem').innerText;
+                    problem = problemNumber.toString;
+                    let number = document.getElementById('currentScore').innerText;
+                    number = numberCorrect.toString;
+                }
+                else {
+                    problemNumber+=1;
+                    let problem = document.getElementById('currentProblem').innerText;
+                    problem = problemNumber.toString;
+                    
+                }
+            }))
+            // buildNewProblem();
         }
+
+    // function gamePlay(){
+    //     let problemNumber = 1;
+    //     let numberCorrect = 0;
+    //     for(let i = 0; i<10; i++){
+    //         buildNewProblem();
+    //         let listOfAnswers = document.getElementById('answers').querySelector('ul').querySelectorAll('li');
+    //         listOfAnswers.forEach(element => element.addEventListener('click', (event) => {
+    //             console.log(event.target.innerText)
+    //             if(event.target.innerText == correctAnswer){
+    //                 event.target.setAttribute('class', 'correct-answer');
+    //                 numberCorrect += 1;
+    //                 problemNumber += 1;
+    //             }
+    //             else {
+    //                 problemNumber+=1;
+    //             }
+    //         }))
+    //     }
+    // }
+
 
 
        function getRandomNumber() {
@@ -79,6 +117,15 @@
 
        function shuffleArray(arr) {
         return arr.sort(function (a, b) { return Math.random() - 0.5 })
+      }
+
+      function createAnswerArray(){
+           //Initialize array containing correct answers and 3 wrong answers
+        let answers = [correctAnswer, getRandomNumber(), getRandomNumber(), getRandomNumber()];
+
+        //Randomize order of possible answers in array
+        let array = shuffleArray(answers);
+        return array;
       }
 
     //   function displayAnswer(answer) {
